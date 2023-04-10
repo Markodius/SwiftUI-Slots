716e768f7481d2cf8f4d56f4f432d2c1d9dbf278
mykola <mykola.sosula@gmail.com>
2023-04-10 13:19:01 +0300

    Initial Commit

---
+++ SwiftUI Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI Slots
+//
+//  Created by Mykola on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
